1519cbe0eae014e70ba37b2b30fb62be59d548f0
Илья Дуксин <you@example.com>
2022-04-23 19:32:13 +0300

    nomer2

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -4,6 +4,7 @@ const fs = require("fs")
 
 module.exports = function (app) {
 
+//проверка на папку.
     function isFolder(path){
         return fs.lstatSync(path).isDirectory() && fs.existsSync(path);
     }
@@ -11,12 +12,34 @@ module.exports = function (app) {
     app.get('/', (req, res) => {  
         //обращение к странице методом get - запрос лежит в request - ответ в response /
     const base = './files/';
-    let path = '';
+    let path = ''; // Папка, которая нужна пользователю.
+
+        if ('path' in req.query){
+            path = req.query.path;
+        }
 
         if ( isFolder(base + path)) {
             // если переданный параметр - папка
-            let files = fs.readdirSync(base+path);
-            res.json(files);
+            let files = fs.readdirSync(base+path).map(item => {
+                const isDir = fs.statSync(base+path+ '/'+item).isDirectory(); // Является ли текущий адрес Директорией?
+                let size = 0;
+
+                if (!isDir) { 
+                    size = fs.statSync(base+path+'/'+item);
+                    console.log(size.size);
+                }
+
+                return { // получаем массив данных о файле
+                    name: item,
+                    dir: isDir,
+                    size: size.size ?? 0
+                }
+            })
+            res.json({
+                path: path, // где мы находимся
+                result: true, // проверка на ошибку
+                files: files // обработанные файлы
+            });
         }
 
     });
